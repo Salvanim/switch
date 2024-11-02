93306eb22581c407451c0a29a35ce4f44e676326
Salvanim <dylanfish05@icloud.com>
2024-11-02 08:48:39 -0400

    switch

--- switch/src/switchObject/__init__.py
+++ switch/src/switchObject/__init__.py
@@ -1,22 +1,30 @@
 class switch:
-    def __init__(self, *caseOutPairs, end=None):
+    def __init__(self, *caseOutPairs, end=None, isItterable=False):
         self.dictionary = {}
-        # Check for odd caseOutPairs length and set the last as end if needed
-        if len(caseOutPairs) % 2 != 0:
-            self.end = caseOutPairs[-1]
-            caseOutPairs = caseOutPairs[:-1]  # Remove last item from cases
-        else:
-            self.end = end  # Use specified end if provided
+        if isItterable: #Used to determine weather or not caseOutPairs is being passed as a itterable
+            caseOutPairs = caseOutPairs[0]
+            #if isItterable is true and caseOutPairs is longer than a length of 1, other cases will be ignored
 
-        # Populate the dictionary with case-output pairs
-        for i in range(0, len(caseOutPairs), 2):
-            case = caseOutPairs[i]
-            output = caseOutPairs[i + 1]
-            self.dictionary[case] = output
+        if type(caseOutPairs) == dict: #handle dictionary cases by seting stored dictionary to caseOutPairs
+            self.dictionary = caseOutPairs
+
+        else:
+            if len(caseOutPairs) % 2 != 0: #Check if caseOutPairs list length is even
+                self.end = caseOutPairs[-1]
+                caseOutPairs = caseOutPairs[:-1]  # Remove last item from cases
+            else:
+                self.end = end  # Use specified end if provided
+
+            # Populate the dictionary with case-output pairs
+            for i in range(0, len(caseOutPairs), 2):
+                case = caseOutPairs[i]
+                output = caseOutPairs[i + 1]
+                self.dictionary[case] = output
 
     def __call__(self, matchCase, *args, **kwargs):
         if matchCase in self.dictionary:
             result = self.dictionary[matchCase]
+            
             # If result is a switch, recursively call it
             if isinstance(result, switch):
                 return result(matchCase, *args, **kwargs)
